e9920179b0e987bfcce8b26778a0945bdbf7bb83
santhosh <santhobs@gmail.com>
2026-02-18 15:02:18 +0530

    Login & Forgot Password design updated

--- src/app/pages/auth/login.component.ts
+++ src/app/pages/auth/login.component.ts
@@ -1,49 +1,255 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { AuthService } from '../../core/services/auth.service';
+import {Component,OnInit,OnDestroy} from '@angular/core';
+import {FormBuilder,Validators,ReactiveFormsModule,FormGroup} from '@angular/forms';
+import {Router,RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {AuthService} from '../../core/services/auth.service';
+
+
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+
+selector: 'app-login',
+standalone: true,
+
+imports: [
+CommonModule,
+ReactiveFormsModule,
+RouterModule
+],
+
+templateUrl: './login.component.html',
+styleUrls: ['./login.component.scss']
+
 })
-export class LoginComponent {
 
-  loading = false;
-  error = '';
-  form!: FormGroup;
+export class LoginComponent
+implements OnInit, OnDestroy {
 
 
-  constructor(
-    private fb: FormBuilder,
-    private auth: AuthService,
-    private router: Router
-  ) { }
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
+// LOGIN
+loading = false;
+error = '';
+form!: FormGroup;
 
-  submit() {
-    if (this.form.invalid) return;
+// CAROUSEL
+currentSlide = 0;
+intervalId: any;
 
-    this.loading = true;
-    this.error = '';
 
-    this.auth.login(this.form.value).subscribe({
-      next: () => {
-        this.router.navigate(['/dashboard']);
-      },
-      error: err => {
-        this.error = err.error?.message || 'Login failed';
-        this.loading = false;
-      }
-    });
-  }
+// swipe
+startX = 0;
+endX = 0;
+isDragging = false;
+
+slides = [
+{
+image: '/images/product/machine_login_02.png',
+title: 'Access your IoT Dashboard',
+desc: 'Monitor machines and production live'
+},
+
+{
+image: '/images/product/machine_login_02.png',
+title: 'Track OEE Performance',
+desc: 'Availability, Quality and Efficiency'
+},
+
+{
+image: '/images/product/machine_login_02.png',
+title: 'Smart Factory Control',
+desc: 'Real time monitoring and alerts'
+}
+
+];
+
+
+
+constructor(
+private fb: FormBuilder,
+private auth: AuthService,
+private router: Router
+) {}
+
+isDark = false;
+
+
+////////////////////////////////////////////
+// INIT
+////////////////////////////////////////////
+
+ngOnInit(): void {
+this.form = this.fb.group({
+email: ['', Validators.required],
+password: ['', Validators.required]
+});
+
+
+this.startCarousel();
+this.isDark =
+document.documentElement.classList.contains('dark');
+}
+
+////////////////////////////////////////////
+// TOGGLE DARK & LIGHT
+////////////////////////////////////////////
+
+toggleDark()
+{
+this.isDark = !this.isDark;
+if(this.isDark)
+{
+document.documentElement.classList.add('dark');
+localStorage.setItem('theme','dark');
+}
+else
+{
+document.documentElement.classList.remove('dark');
+localStorage.setItem('theme','light');
+}
+
+}
+
+////////////////////////////////////////////
+// DESTROY
+////////////////////////////////////////////
+
+ngOnDestroy(): void {
+this.pauseCarousel();
+}
+
+
+
+////////////////////////////////////////////
+// CAROUSEL AUTO PLAY
+////////////////////////////////////////////
+
+startCarousel() {
+this.pauseCarousel();
+this.intervalId = setInterval(() => {
+this.nextSlide();
+}, 4000);
+}
+
+
+pauseCarousel() {
+if (this.intervalId) {
+clearInterval(this.intervalId);
+}
+}
+
+
+resumeCarousel() {
+this.startCarousel();
+}
+
+
+
+////////////////////////////////////////////
+// SLIDE CONTROL
+////////////////////////////////////////////
+
+nextSlide() {
+this.currentSlide =
+(this.currentSlide + 1)
+% this.slides.length;
+}
+
+
+prevSlide() {
+this.currentSlide =
+(this.currentSlide - 1 + this.slides.length)
+% this.slides.length;
+}
+
+
+goToSlide(index:number) {
+this.currentSlide = index;
+}
+
+
+
+////////////////////////////////////////////
+// TOUCH EVENTS
+////////////////////////////////////////////
+
+onTouchStart(event:any) {
+this.startX =
+event.touches[0].clientX;
+}
+
+
+onTouchEnd(event:any) {
+this.endX =
+event.changedTouches[0].clientX;
+this.handleSwipe();
+}
+
+
+////////////////////////////////////////////
+// MOUSE EVENTS
+////////////////////////////////////////////
+
+onMouseDown(event:any) {
+this.isDragging = true;
+this.startX = event.clientX;
+}
+
+
+onMouseUp(event:any) {
+if (!this.isDragging) return;
+this.isDragging = false;
+this.endX = event.clientX;
+this.handleSwipe();
+}
+
+
+
+////////////////////////////////////////////
+// SWIPE LOGIC
+////////////////////////////////////////////
+
+handleSwipe() {
+const diff =
+this.startX - this.endX;
+
+if (Math.abs(diff) < 50)
+return;
+
+if (diff > 0)
+this.nextSlide();
+else
+this.prevSlide();
+}
+
+
+
+////////////////////////////////////////////
+// LOGIN
+////////////////////////////////////////////
+
+submit() {
+if (this.form.invalid)
+return;
+this.loading = true;
+this.error = '';
+
+this.auth.login(this.form.value)
+.subscribe({
+
+next: () => {
+this.router.navigate(['/dashboard']);
+},
+
+error: err => {
+this.error =
+err.error?.message ||
+'Login failed';
+this.loading = false;
+}
+
+});
+
+}
+
+
 }
--- src/app/pages/forgot/forgot-password.component.ts
+++ src/app/pages/forgot/forgot-password.component.ts
@@ -1,42 +1,193 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
-import { AuthService } from "../../core/services/auth.service";
-import { RouterModule } from "@angular/router";
+import {Component,OnInit,OnDestroy} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {ReactiveFormsModule,FormGroup,FormControl,Validators} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AuthService} from "../../core/services/auth.service";
+
+
 
 @Component({
-  standalone: true,
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  imports: [CommonModule, ReactiveFormsModule,RouterModule],
-  styleUrls: ['./forgot-password.component.scss']
+
+selector: 'app-forgot-password',
+standalone: true,
+
+imports: [
+CommonModule,
+ReactiveFormsModule,
+RouterModule
+],
+
+templateUrl: './forgot-password.component.html',
+styleUrls: ['./forgot-password.component.scss']
 
 })
-export class ForgotPasswordComponent {
 
-  loading = false;
-  message = '';
+export class ForgotPasswordComponent
+implements OnInit, OnDestroy
+{
 
-  form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email])
-  });
+loading = false;
+message = '';
+isDark = false;
 
-  constructor(private auth: AuthService) { }
 
-  submit() {
-    if (this.form.invalid) return;
+////////////////////////////////////////////
+// FORM
+////////////////////////////////////////////
 
-    this.loading = true;
-    this.auth.forgotPassword(this.form.value.email!)
-      .subscribe({
-        next: (res: any) => {
-          this.message = res.message;
-          this.loading = false;
-        },
-        error: () => {
-          this.message = 'Something went wrong';
-          this.loading = false;
-        }
-      });
-  }
+form = new FormGroup({
+email: new FormControl('',
+[Validators.required, Validators.email])
+});
+
+
+////////////////////////////////////////////
+// CAROUSEL
+////////////////////////////////////////////
+
+currentSlide = 0;
+intervalId:any;
+
+slides = [
+{
+image: '/images/product/machine_login_02.png',
+title: 'Reset Password Securely',
+desc: 'Recover access to your dashboard'
+},
+
+{
+image: '/images/product/machine_login_02.png',
+title: 'Secure Authentication',
+desc: 'Your data stays protected'
+},
+
+{
+image: '/images/product/machine_login_02.png',
+title: 'Back to Production Fast',
+desc: 'Minimize downtime quickly'
+}
+
+];
+
+
+
+constructor(
+private auth:AuthService
+){}
+
+
+////////////////////////////////////////////
+// INIT
+////////////////////////////////////////////
+
+ngOnInit(): void
+{
+
+this.startCarousel();
+this.isDark =
+document.documentElement.classList.contains('dark');
+}
+
+
+
+////////////////////////////////////////////
+// DESTROY
+////////////////////////////////////////////
+
+ngOnDestroy(): void
+{
+this.pauseCarousel();
+}
+
+
+////////////////////////////////////////////
+// DARK MODE
+////////////////////////////////////////////
+
+toggleDark()
+{
+this.isDark = !this.isDark;
+if(this.isDark)
+{
+document.documentElement.classList.add('dark');
+localStorage.setItem('theme','dark');
+}
+else
+{
+
+document.documentElement.classList.remove('dark');
+localStorage.setItem('theme','light');
+}
+
+}
+
+
+////////////////////////////////////////////
+// CAROUSEL
+////////////////////////////////////////////
+
+startCarousel()
+{
+
+this.pauseCarousel();
+this.intervalId = setInterval(() =>
+{
+this.nextSlide();
+},4000);
+
+}
+
+pauseCarousel()
+{
+if(this.intervalId)
+clearInterval(this.intervalId);
+}
+
+
+nextSlide()
+{
+this.currentSlide =
+(this.currentSlide+1)
+% this.slides.length;
+}
+
+goToSlide(index:number)
+{
+this.currentSlide = index;
+}
+
+
+////////////////////////////////////////////
+// SUBMIT
+////////////////////////////////////////////
+
+submit()
+{
+if(this.form.invalid)
+return;
+this.loading = true;
+this.auth
+.forgotPassword(this.form.value.email!)
+.subscribe({
+next:(res:any)=>
+{
+this.message =
+res.message ||
+'Reset link sent successfully';
+this.loading = false;
+},
+
+error:()=>
+{
+
+this.message =
+'Something went wrong';
+this.loading = false;
+
+}
+
+});
+
+}
+
 }
