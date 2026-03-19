75ede7a761bda422333f2f635c99c45726ff2b15
santhosh <santhobs@gmail.com>
2026-03-19 19:11:58 +0530

    Dashboard Design Fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-    apiUrl: 'http://localhost:8000/api',
-  // apiUrl: 'http://13.205.238.163:8000/api',
+    // apiUrl: 'http://localhost:8000/api',
+  apiUrl: 'http://13.205.238.163:8000/api',
   frontendUrl: 'http://localhost:4200',
-    socketUrl: 'http://localhost:8000'
-
+    // socketUrl: 'http://localhost:8000'
+socketUrl: 'http://13.205.238.163:8000',
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ module.exports = {
       fontFamily: {
         ubuntu: ['Ubuntu', 'sans-serif'],
         franklin: ['Libre Franklin', 'sans-serif'],
-      }
+      },
+      keyframes: {
+    blink: {
+      '0%, 100%': { borderColor: 'transparent' },
+      '50%': { borderColor: '#fb3748' }, // red
+    },
+    borderMove: {
+      '0%': { backgroundPosition: '0% 50%' },
+      '100%': { backgroundPosition: '100% 50%' },
+    },
+  },
+  animation: {
+    blink: 'blink 1s infinite',
+    borderMove: 'borderMove 3s linear infinite',
+  }
     },
   },
   plugins: [],
