74fcf0278d49bfc68ec3b93f66aa6b13a9deddcb
Gokul <gokuldsp01@email.com>
2026-03-23 21:18:05 +0530

    report page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://13.205.238.163:8000/api',
-  frontendUrl: 'https://13.205.238.163',
-  socketUrl: 'https://13.205.238.163:8000'
+  apiUrl: 'http://13.205.238.163:8000/api',
+  frontendUrl: 'http://sandtiot.s3-website.ap-south-1.amazonaws.com',
+  socketUrl: 'http://13.205.238.163:8000'
 };
