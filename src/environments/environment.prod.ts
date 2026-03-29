01303a1353a7011c523e06d9578993e319b02a4c
Gokul <gokuldsp01@email.com>
2026-03-29 11:21:16 +0530

    domain added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://stmapi.stmcnc.com/api',
-  frontendUrl: 'http://sandtiot.s3-website.ap-south-1.amazonaws.com',
+  frontendUrl: 'https://stmmexa.stmcnc.com',
   socketUrl: 'https://stmapi.stmcnc.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8000/api',
   apiUrl: 'https://stmapi.stmcnc.com/api',
-  frontendUrl: 'http://localhost:4200',
+  frontendUrl: 'https://stmmexa.stmcnc.com',
   // socketUrl: 'http://localhost:8000'
   socketUrl: 'https://stmapi.stmcnc.com'
 };
