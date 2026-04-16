a64056b89401d88dc1004356dc7f227e01d9a6cd
santhosh <santhobs@gmail.com>
2026-04-16 18:02:56 +0530

    design fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api',
-  frontendUrl: 'http://localhost:4200',
-  socketUrl: 'http://localhost:8000'
-  // apiUrl: 'https://stmapi.stmcnc.com/api',
-  // frontendUrl: 'https://stmmexa.stmcnc.com',
-  // socketUrl: 'https://stmapi.stmcnc.com'
+  // apiUrl: 'http://localhost:8000/api',
+  // frontendUrl: 'http://localhost:4200',
+  // socketUrl: 'http://localhost:8000'
+  apiUrl: 'https://stmapi.stmcnc.com/api',
+  frontendUrl: 'https://stmmexa.stmcnc.com',
+  socketUrl: 'https://stmapi.stmcnc.com'
 };
